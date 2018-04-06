f43bfb0a830f02f7dc697fed46bccd45cd28e200
sunburn21 <sunnythesingh21@gmail.com>
2018-04-06 22:25:46 +0530

    browse&logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,10 @@
 const express=require('express');
 const router=express.Router();
 
-const {registerform,signUp,login,loginpost}=require('../helperFunctions/authFunctions')
+const {registerform,signUp,login,loginpost,logout}=require('../helperFunctions/authFunctions')
 empty=function (req, res) {}
 router.route('/register').get(registerform).post(signUp);
 
 router.route('/login').get(login).post(loginpost(),empty);
-
+router.route("/logout").get(logout)
 module.exports=router;
