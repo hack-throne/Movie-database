d2b124ff73bb56f01c680267aa2d538d03fe2cc3
anshul goyal <anshulgoel151999@gmail.com>
2018-04-20 19:20:54 +0530

    bug fixed and picture fixed code refactored

--- helperFunctions/navBarFunction.js
+++ helperFunctions/navBarFunction.js
@@ -23,37 +23,12 @@ const getAbout=function (req, res){
     res.render('about.ejs')
 }
 const getBrowse=function (req, res) {
-    let scrap=[];
     const genre=['action','comedy','fiction','romantic','thriller']
     let movies=genre.map((one)=>{
         return getTrendingGenre(one,8)
     })
     Promise.all(movies).then((value)=>{
-        scrap=value;
-       let movies=[];
-       movies= movies.concat(...value);
-       movies=movies.map((each)=>{
-           return each.trending
-       })
-        movies=[].concat(...movies);
-      console.log(movies.length)
-       let result= movies.map((key)=>{
-            return request.get(`https://www.omdbapi.com/?i=${key.id}&apikey=1799f783`)
-        })
-        return Promise.all(result).then((data)=>{
-            console.log(data.length)
-            let ob={};
-            data.forEach((pic)=>{
-                pic=JSON.parse(pic);
-               // console.log(ob);
-                ob[pic.imdbID]=pic.Poster
-            });
-            console.log(Object.keys(ob).length)
-            return ob
-        })
-
-    }).then((value)=>{
-        res.render('browse.ejs',{images:value,movies:scrap});
+        res.render('browse.ejs',{movies:value});
     })
 
 };
